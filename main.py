faeec0c4a1e5b873feed77fba2dd62b36ffe9675
Pille9 <154467092+Pille9@users.noreply.github.com>
2025-02-12 19:45:30 +0000

    commit11

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 
 def main():
+    st.markdown(
+    """
+    <style>
+    body {
+        background-image: url("https://es.akinator.com/assets/img/akitudes_520x650/defi.png");
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
     st.title('Adivina el género musical')
     st.write('**Cómo lo quieres adivinar**')
-    imagen = Image.open('/workspaces/jvbd-streamlit/akinator.webp')
+    imagen = Image.open('akinator.webp')
     st.image(imagen, use_container_width=True)
     opcion = st.radio('Selecciona cómo adivinar:', 
                       ('Audio', 'Carátula', 'Cantante'), 
